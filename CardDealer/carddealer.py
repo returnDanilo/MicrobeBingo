7261acab1cc4b19fe7c988abf4ef2283492fdbbc
Danilo Rezende <returndanilo@users.noreply.github.com>
2024-10-08 16:11:59 -0300

    use gpt-4o instead of gpt-4o-ini

--- CardDealer/carddealer.py
+++ CardDealer/carddealer.py
@@ -127,7 +127,7 @@ class MyBot(commands.Bot):
 				for i in range(10):
 					try:
 						completion = client.chat.completions.create(
-							model="gpt-4o-mini",
+							model="gpt-4o",
 							messages=[{"role":"user","content": [
 									{"type":"text","text": TEXT_PROMPT},
 									{"type":"image_url","image_url":{"url":f"data:{IMG_MIMETYPE};base64,{image_prompt}","detail":"high"},},],}],)
